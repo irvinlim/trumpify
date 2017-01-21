324756baf3086468d82b5715308439836b075770
Kent <kent@moneylover.me>
2017-01-21 21:41:59 +0800

    remove index

--- js/trumpify_avatars.js
+++ js/trumpify_avatars.js
@@ -73,3 +73,18 @@ let trumpifyAvatars = () => {
     }
 
 };
+
+document.body.addEventListener('DOMNodeInserted', () => {
+    // Don't observe so often
+    if (window.__trumpifyPageJustLoaded) {
+        return;
+    }
+
+    // Timeout to allow re-observing
+    setTimeout(() => {
+        window.__trumpifyPageJustLoaded = false;
+    }, 500);
+
+    trumpifyAvatars();
+});
+
