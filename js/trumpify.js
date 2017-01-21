0fcca8d17e50156da86a5aa0d862f078db902fd8
Irvin Lim <irvinlimweiquan@gmail.com>
2017-01-21 19:13:54 +0800

    Remove usage of MutationObserver

--- js/trumpify.js
+++ js/trumpify.js
@@ -1,111 +1,55 @@
-// Declare specifications for containers to find images in, and containers to bind to to detect changes
-let containerSpecs = [
-    {
-        selectors: [
-            'div[role="navigation"] div[data-click="profile_icon"] a',
-            '#feedx_container a'
-        ]
-    },
-    {
-        listenOn: '.tickerActivityStories',
-        selectors: [ '.tickerStoryBlock' ],
-    },
-    {
-        listenOn: 'div[id^="topnews_main_stream"]',
-        selectors: [ 'a[data-hovercard]', '.UFIImageBlockImage', '.profilePic' ]
-    },
-    {
-        listenOn: 'div[id^="more_pager_pagelet"]',
-        selectors: [ 'a[data-hovercard]', '.UFIImageBlockImage', '.profilePic' ]
-    },
-    {
-        listenOn: 'div[data-testid="chat_sidebar"]',
-        selectors: [ 'li a' ],
-    },
-    {
-        listenOn: 'body',
-        selectors: [
-            '.uiContextualLayerPositioner a[data-hovercard]',
-            '.uiContextualLayerPositioner .UFIImageBlockImage',
-            '.uiContextualLayerPositioner td a',
-            '.uiContextualLayerPositioner _dynamicHovercard__socialContextRow',
-            '.uiContextualLayerPositioner .profilePic',
-        ],
-    },
-];
+// Declare selectors
+let selectorConfig = {
 
-// Method to set pictures, given an array of selectors
-let setPictures = (selectors, enclosing) => {
-    if (!enclosing.querySelectorAll) {
+    profilePictureSelectors: [
+        'div[role="navigation"] div[data-click="profile_icon"] a',
+        '#feedx_container a',
+        '.tickerActivityStories .tickerStoryBlock',
+        'a[data-hovercard]',
+        '.UFIImageBlockImage',
+        '.profilePic',
+        'div[data-testid="chat_sidebar"] li a',
+        '.uiContextualLayerPositioner a[data-hovercard]',
+        '.uiContextualLayerPositioner .UFIImageBlockImage',
+        '.uiContextualLayerPositioner td a',
+        '.uiContextualLayerPositioner _dynamicHovercard__socialContextRow',
+        '.uiContextualLayerPositioner .profilePic',
+    ],
+
+};
+
+// Method to set pictures, given a selector
+let setPictures = (selector) => {
+    let selectedBlocks = document.querySelectorAll(selector);
+
+    if (!selectedBlocks) {
         return;
     }
 
-    selectors.forEach(selector => {
-        let blocks = enclosing.querySelectorAll(selector);
+    for (let block of selectedBlocks) {
+        let img = block.querySelector('img');
 
-        if (!blocks) {
-            return;
+        if (img && !img.dataset.trumpify) {
+            // Set flag
+            img.dataset.trumpify = true;
+
+            // Update image src
+            img.src = "https://lh5.googleusercontent.com/-Sv0q6lkSAGM/AAAAAAAAAAI/AAAAAAAAAYI/eE7kV9LMcnQ/s0-c-k-no-ns/photo.jpg";
         }
-
-        for (let block of blocks) {
-            let img = block.querySelector('img');
-
-            if (img && !img.dataset.trumpify) {
-                img.src = "https://lh5.googleusercontent.com/-Sv0q6lkSAGM/AAAAAAAAAAI/AAAAAAAAAYI/eE7kV9LMcnQ/s0-c-k-no-ns/photo.jpg";
-                img.dataset.trumpify = true;
-            }
-        }
-    });
+    }
 };
 
 let trumpify = () => {
 
-    // Bind event listeners
-    for (let containerSpec of containerSpecs) {
-        let { selectors, listenOn } = containerSpec;
+    let { profilePictureSelectors } = selectorConfig;
 
-        if (!listenOn) {
-            continue;
-        }
-
-        for (let listenContainer of document.querySelectorAll(listenOn)) {
-            let timer;
-
-            let observer = new MutationObserver(mutations => {
-                mutations.forEach(mutation => {
-                    mutation.addedNodes.forEach(node => {
-                        setPictures(selectors, node);
-                    });
-                });
-            });
-
-            let observeConfig = {
-                childList: true,
-                subtree: true,
-            };
-
-            observer.observe(listenContainer, observeConfig);
-        }
-    }
-
-    // Load immediately
-    for (let containerSpec of containerSpecs) {
-        let { selectors, listenOn } = containerSpec;
-        let containers;
-
-        if (listenOn) {
-            containers = document.querySelectorAll(listenOn);
-        } else {
-            containers = [ document ];
-        }
-
-        containers.forEach(container => {
-            setPictures(selectors, container);
-        });
+    // Trumpify profile pictures
+    for (let selector of profilePictureSelectors) {
+        setPictures(selector);
     }
 
 };
 
 // Use setInterval to repeatedly make Facebook great again and again and again...
 // There is no easy way to detect page changes without access to the internal React code.
-setInterval(trumpify, 100);
+setInterval(trumpify, 50);
