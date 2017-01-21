06fbdccd608ffefe16a050fb7d4b93bb9011f193
Irvin Lim <irvinlimweiquan@gmail.com>
2017-01-22 03:10:15 +0800

    Use correct icon for tab change

--- js/background.js
+++ js/background.js
@@ -1,10 +1,14 @@
 function updateIcon() {
-    chrome.tabs.getSelected(null, function (tab) {
-        if (tab.url && tab.url.match(/https:\/\/.*\.facebook\.com\/.*/)) {
-            chrome.browserAction.setIcon({ path: '../img/icons/icon_16.png', tabId: tab.id });
-        } else {
-            chrome.browserAction.setIcon({ path: '../img/icons/icon_16_grey.png', tabId: tab.id });
-        }
+    chrome.storage.local.get({ "isEnabled": true }, function ({ isEnabled }) {
+        chrome.tabs.getSelected(null, function (tab) {
+            if (!tab.url || !tab.url.match(/https:\/\/.*\.facebook\.com\/.*/)) {
+                chrome.browserAction.setIcon({ path: '../img/icons/icon_16_grey.png', tabId: tab.id });
+            } else if (!isEnabled) {
+                chrome.browserAction.setIcon({ path: '../img/icons/icon_16_disabled.png', tabId: tab.id });
+            } else {
+                chrome.browserAction.setIcon({ path: '../img/icons/icon_16.png', tabId: tab.id });
+            }
+        });
     });
 }
 
