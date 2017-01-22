11ab070e13411011cec7e2c03e3fcff99fde7871
Irvin Lim <irvinlimweiquan@gmail.com>
2017-01-22 08:16:41 +0800

    Add GA

--- js/background.js
+++ js/background.js
@@ -17,3 +17,16 @@ chrome.tabs.onSelectionChanged.addListener(function (tabId, props) {
 });
 
 updateIcon();
+
+// Google Analytics
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-90668173-1']);
+
+// Send one page view per browser session in the background
+_gaq.push(['_trackPageview']);
